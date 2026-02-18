ecc6636ed40851bb6e90a21e073d73faa771bc77
kritagyadahal588@gmail.com <80688932+KTN2757@users.noreply.github.com>
2026-02-18 15:32:58 +0545

    Added variables and equal sign

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import string
+
+
 class Token:
     def __init__(self, type, value, pos):
         self.type = type
@@ -8,9 +11,10 @@ class Token:
 class Tokenizer:
     def __init__(self, text):
         self.text = text
-        self.DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.DIGITS = string.digits
         self.SYMBOLS = {"+": "PLUS", "-": "MINUS", "*": "STAR",
-                        "/": "SLASH", "(": "LPAREN", ")": "RPAREN"}
+                        "/": "SLASH", "(": "LPAREN", ")": "RPAREN", "=": "EQUAL"}
+        self.LETTERS = string.ascii_letters
         self.tokens = []
 
     def run(self):
@@ -18,22 +22,46 @@ class Tokenizer:
 
     def tokenize(self):
         num_buf = ""
+        letter_buf = ""
         num_start = 0
+        letter_start = 0
         for i in range(len(self.text)):
-            if self.text[i] in self.DIGITS or self.text[i] == ".":
+            if self.text[i] in self.DIGITS or (self.text[i] == "." and num_buf):
+                if letter_buf:
+                    self.tokens.append(Token("VAR", letter_buf, letter_start))
+                    letter_buf = ""
                 if not num_buf:
                     num_start = i
                 num_buf += self.text[i]
+
+            elif self.text[i] in self.LETTERS:
+                if num_buf:
+                    self.tokens.append(Token("NUMBER", num_buf, num_start))
+                    num_buf = ""
+                if not letter_buf:
+                    letter_start = i
+                letter_buf += self.text[i]
 
             else:
                 if num_buf:
                     self.tokens.append(Token("NUMBER", num_buf, num_start))
                     num_buf = ""
+                if letter_buf:
+                    self.tokens.append(Token("VAR", letter_buf, letter_start))
+                    letter_buf = ""
                 if self.text[i] in self.SYMBOLS:
                     self.tokens.append(
                         Token(self.SYMBOLS[self.text[i]], self.text[i], i))
+                elif self.text[i] not in (" ", "\t", "\n"):
+                    self.tokens.append(Token("ILLEGAL", self.text[i], i))
+
             if self.text[i] == "\n":
                 self.tokens.append(Token("END", self.text[i], i))
+
+        if num_buf:
+            self.tokens.append(Token("NUMBER", num_buf, num_start))
+        if letter_buf:
+            self.tokens.append(Token("VAR", letter_buf, letter_start))
 
     def list_tokens(self):
         for i in self.tokens:
